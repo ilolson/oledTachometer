97bdda2053d9a2e85544e29e757825fdd287c907
Isaac Olson <125952796+ilolson@users.noreply.github.com>
2024-02-20 01:12:28 -0600

    v1

---
+++ TachAddrLED/TachAddrLED.ino
@@ -0,0 +1,142 @@
+// Isaac Olson Addressable LED Tachometer
+// Must be connected to vehicle ground to function correctly
+// Board compatibility may vary, Originally designed to work on the raspberry pi pico
+// Pin 6 is the signal pin
+// Pin 4 is the brightness cycle pin
+
+#include <FastLED.h>
+#define NUM_LEDS 8
+#define DATA_PIN 0
+CRGB leds[NUM_LEDS];
+
+/* PARAMETERS */
+const unsigned long resetTime = 100;  // in milliseconds low long after the last signal do you want the gauge to decay its value
+const double decayRate = 0.998;         // Adjust this value for the rate of decay (0.995 means 0.5% decay per loop )
+const int numReadings = 10;             // This is how 'stable' the rpm value is, adjust this to smooth out values
+double multiplier = 30000.0;           // 30000.0 for 2 signals per rev, 60000.0 for 1 signal per rev, 120000.0 for 0.5 signal per rev
+const int redline = 5;                  // 5 is a 6000RPM redline, this determines when the gauge will turn all previous LEDS red (redline rpm / 1000) - 1,
+const int maxRPM = 7999;              // Adjust the maximum rpm the display will read, default 7999, recomended max 7999
+
+volatile unsigned long ignCount = 0;
+unsigned long lastTime = 0;
+unsigned long currentTime = 0;
+unsigned long timeDiff = 0;
+int displayRefresh = 1;
+unsigned long readings[numReadings];
+unsigned long total = 0;
+double rollingAvgGra = 7999;
+double rpm = 7999;
+int indexAVG = 0;
+unsigned long lastInputTime = 0;
+int brightnessState = 0;
+double rollingAvgDis = 0;
+unsigned long elapsedTime;
+void changeBrightness() {
+  if (digitalRead(4) == LOW) {
+    if (brightnessState == 5) {
+      brightnessState = 0;
+    } else {
+      brightnessState++;
+    }
+    if (brightnessState == 0) {
+      FastLED.setBrightness(10);
+    } else if (brightnessState == 1) {
+      FastLED.setBrightness(25);
+    } else if (brightnessState == 2) {
+      FastLED.setBrightness(50);
+    } else if (brightnessState == 3) {
+      FastLED.setBrightness(100);
+    } else if (brightnessState == 4) {
+      FastLED.setBrightness(200);
+    } else if (brightnessState == 5) {
+      FastLED.setBrightness(3);
+    }
+    delay(333);
+  }
+}
+
+void isr() {
+  ignCount++;
+  lastInputTime = millis();
+}
+
+void update() {
+  currentTime = millis();
+  timeDiff = currentTime - lastTime;
+  if (ignCount > 1) {
+    rpm = (multiplier) / timeDiff * ignCount;
+    ignCount = 0;
+    updateRollingAverage(rpm);
+    displayRefresh = 1;
+    lastTime = millis();
+  }
+}
+
+void updateRollingAverage(double rpm) {
+  total -= readings[indexAVG];
+  readings[indexAVG] = rpm;
+  total += rpm;
+  indexAVG = (indexAVG + 1) % numReadings;
+  rollingAvgGra = total / numReadings;
+  rollingAvgGra = constrain(rollingAvgGra, 0, maxRPM);
+  rollingAvgDis = rollingAvgGra / 1000.0;
+}
+
+void setup() {
+  delay(100);
+  Serial.begin(9600);
+  FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.show();
+  FastLED.setBrightness(10);
+  pinMode(6, INPUT_PULLDOWN);
+  pinMode(4, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(6), isr, FALLING);
+}
+
+void loop() {
+
+  changeBrightness();
+  elapsedTime = millis() - lastInputTime;
+  if (elapsedTime > resetTime && rollingAvgGra > 0) {
+    rpm *= decayRate;
+    updateRollingAverage(rpm);
+    displayRefresh = 1;
+  }
+  update();
+  if (displayRefresh == 1) {
+    displayRefresh = 0;
+    int krpm = rollingAvgGra / 1000;
+    for (int i = 0; i <= 7; i++)
+      if (i < krpm) {
+        if (krpm > redline) {
+          leds[i] = CRGB::Red;
+        } else {
+
+          // change color here
+
+          // leds[i] = CRGB::Red;
+          leds[i] = CRGB::Green;
+          // leds[i] = CRGB::Blue;
+          // leds[i] = CRGB::Purple;
+          // leds[i] = CRGB::Aqua;
+          // leds[i] = CRGB::Black;
+          // leds[i] = CRGB::White;
+        }
+      } else {
+        leds[i] = CRGB::Black;
+      }
+    double hrpm = fmod(rollingAvgGra, 1000.0);
+
+    // continue changing color here
+
+    // int hue = map(hrpm,0,1000,80,255); // red
+    int hue = map(hrpm, 0, 1000, 255, 80);  // green
+    // int hue = map(hrpm,0,1000,0,170); // blue
+    // int hue = map(hrpm,0,1000,0,215); // Purple
+    // int hue = map(hrpm,0,1000,0,145); // Aqua
+    // int hue = map(hrpm, 0, 1000, 0, 255);  // Black
+
+    leds[krpm].setHue(hue);
+    FastLED.show();
+  }
+}
---
+++ tachOLED/tachOLED.ino
@@ -0,0 +1,125 @@
+// Isaac Olson OLED Tachometer
+// Must be connected to vehicle ground to function correctly
+// Board compatibility may vary, tested on Arduino Nano Every
+// Pin 6 is the signal pin
+
+/* OPTIONS */
+const unsigned long resetTime = 100;  // in millis low long after the last signal do you want the gauge to start decaying its value
+const double decayRate = .8;          // Adjust this value for the rate of decay (0.9 means 10% decay per loop )
+const int numReadings = 10;           // This is how 'stable' the rpm value is, adjust this to smooth out values
+const int maxRPM = 7999;              // Adjust the maximum rpm the display will read, default 7999, recomended max 9999
+const int showShiftInd = 5500;        // The value the onboard LED and shift indicator will turn on
+double multiplier = 300000.0;         // 300000.0 for 2 signals per rev, 600000.0 for 1 signal per rev, 1200000.0 for 0.5 signal per rev
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define SCREEN_ADDRESS 0x3C
+
+#define OLED_RESET -1
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+int barWidth;
+volatile unsigned long ignCount = 0;
+unsigned long lastTime = 0;
+int displayRefresh = 1;
+unsigned long readings[numReadings];
+unsigned long total = 0;
+int rollingAvgGra = 1;
+double rollingAvgDis;
+int rpm = 0;
+int indexAVG = 0;
+unsigned long lastInputTime = 0;
+unsigned long currentTime = 0;
+unsigned long timeDiff = 0;
+unsigned long elapsedTime = 0;
+
+
+void isr() {
+  ignCount++;
+  lastInputTime = millis();
+}
+
+void update() {
+  currentTime = millis();
+  timeDiff = currentTime - lastTime;
+  if (ignCount > 1) {
+    rpm = (multiplier) / timeDiff * ignCount;
+    ignCount = 0;
+    updateRollingAverage(rpm);
+    displayRefresh = 1;
+    lastTime = millis();
+  }
+}
+
+void updateRollingAverage(double rpm) {
+  total -= readings[indexAVG];
+  readings[indexAVG] = rpm;
+  total += rpm;
+  indexAVG = (indexAVG + 1) % numReadings;
+  rollingAvgGra = total / numReadings;
+  rollingAvgGra = constrain(rollingAvgGra, 0, maxRPM);
+  rollingAvgDis = rollingAvgGra / 1000.0;
+}
+
+void displayGraph() {
+  barWidth = map(rollingAvgGra, 0, maxRPM, 1, SCREEN_WIDTH);
+  display.fillRect(0, 0, barWidth, 15, SSD1306_WHITE);
+}
+
+void displayChar() {
+  display.setTextColor(SSD1306_WHITE);
+  display.setTextSize(1);
+  display.setCursor(2, 20);
+  display.print("RPM");
+  if (rollingAvgGra > showShiftInd) {
+    display.setTextSize(4);
+    display.setCursor(2, 32);
+    display.print(rollingAvgDis, 1);
+    display.setTextSize(3);
+    display.print(" ");
+    display.setTextSize(6);
+    display.print("^");
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    display.setTextSize(4);
+    display.setCursor(2, 32);
+    display.print(rollingAvgDis, 3);
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+}
+
+
+void setup() {
+  delay(100);
+  //Serial.begin(115200);
+  Wire.begin();
+  Wire.beginTransmission(SCREEN_ADDRESS);
+  pinMode(6, INPUT_PULLDOWN);
+  pinMode(LED_BUILTIN, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(6), isr, RISING);
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+  }
+}
+
+void loop() {
+
+  elapsedTime = millis() - lastInputTime;
+  if (elapsedTime > resetTime && rollingAvgGra > 0) {
+    rpm *= decayRate;
+    updateRollingAverage(rpm);
+    displayRefresh = 1;
+  }
+  update();
+  if (displayRefresh == 1) {
+    displayRefresh = 0;
+    display.clearDisplay();
+    displayChar();
+    displayGraph();
+    display.display();
+    // Serial.println(rollingAvgGra, 0);
+  }
+}
